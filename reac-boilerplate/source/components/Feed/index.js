cf97228e549f2c3d930c0536da12f80eb2a4df8e
Oleg Lipovoi <oleg@fuseiq.com>
2018-05-29 00:49:08 +0300

    HW_4

--- reac-boilerplate/source/components/Feed/index.js
+++ reac-boilerplate/source/components/Feed/index.js
@@ -5,17 +5,34 @@ import React, { Component } from 'react';
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
 import Post from 'components/Post';
+import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            { id: '1', comment: 'Hi there!', created: 1526825076849 },
+            { id: '2', comment: 'Lets go!', created: 1728825076855 },
+            { id: '3', comment: 'Fuck you!', created: 1936825076949 }
+        ],
+        isSpinning: null,
+    };
+
     render () {
+        const { posts } = this.state;
+
+        const postsJSX = posts.map((post) => {
+            return <Post key = { post.id } { ...post } />;
+        });
+
         return (
             <section className = { Styles.feed }>
+                <Spinner isSpinning />
                 <StatusBar />
                 <Composer />
-                <Post />
+                {postsJSX}
             </section>
         );
     }
