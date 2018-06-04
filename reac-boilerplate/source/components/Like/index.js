df2f705416a27ea114e5cc487310f985a1171e75
Oleg Lipovoi <oleg@fuseiq.com>
2018-06-05 00:30:40 +0300

    HW_6

--- reac-boilerplate/source/components/Like/index.js
+++ reac-boilerplate/source/components/Like/index.js
@@ -6,6 +6,10 @@ import cx from 'classnames';
 // Instruments
 import Styles from './styles.m.css';
 
+// Components
+import { withProfile } from 'components/HOC/withProfile';
+
+@withProfile
 export default class Like extends Component {
     static propTypes = {
         _likePost: func.isRequired,
@@ -18,41 +22,30 @@ export default class Like extends Component {
             }),
         ).isRequired,
     };
-    constructor () {
-        super();
-
-        this._getLikeByMe = this._getLikeByMe.bind(this);
-        this._getLikeStyles = this._getLikeStyles.bind(this);
-        this._likePost = this._likePost.bind(this);
-        this._showLikers = this._showLikers.bind(this);
-        this._hideLikers = this._hideLikers.bind(this);
-        this._getLikersList = this._getLikersList.bind(this);
-        this._getLikesDescription = this._getLikesDescription.bind(this);
-    }
 
     state = {
         showLikers: false,
     }
 
-    _showLikers () {
+    _showLikers = () => {
         this.setState({
             showLikers: true,
         });
     }
 
-    _hideLikers () {
+    _hideLikers = () => {
         this.setState({
             showLikers: false,
         });
     }
 
-    _likePost () {
+    _likePost = () => {
         const { _likePost, id } = this.props;
 
         _likePost(id);
     }
 
-    _getLikeByMe () {
+    _getLikeByMe = () => {
         const { currentUserFirstName, currentUserLastName, likes } = this.props;
 
         return likes.some(({ firstName, lastName }) => {
@@ -63,7 +56,7 @@ export default class Like extends Component {
         });
     }
 
-    _getLikeStyles () {
+    _getLikeStyles = () => {
         const likedByMe = this._getLikeByMe();
 
         return cx(Styles.icon, {
@@ -71,7 +64,7 @@ export default class Like extends Component {
         });
     }
 
-    _getLikersList () {
+    _getLikersList = () => {
         const { showLikers } = this.state;
         const { likes } = this.props;
 
@@ -82,7 +75,7 @@ export default class Like extends Component {
         return likes.length && showLikers ? <ul>{likesJSX}</ul> : null;
     }
 
-    _getLikesDescription () {
+    _getLikesDescription = () => {
         const  { likes, currentUserFirstName, currentUserLastName } = this.props;
 
         const likedByMe =this._getLikeByMe();
--- reac-boilerplate/source/components/Post/index.js
+++ reac-boilerplate/source/components/Post/index.js
@@ -5,11 +5,12 @@ import { func, string, number, array } from 'prop-types';
 
 // Components
 import Like from 'components/Like';
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 
 //instruments
 import Styles from './styles.m.css';
 
+@withProfile
 export default class Post extends Component {
     static propTypes = {
         _likePost:   func.isRequired,
@@ -26,36 +27,31 @@ export default class Post extends Component {
         this._removePost = this._removePost.bind(this);
     }
 
-    _removePost () {
+    _removePost = () => {
         const { _removePost, id } = this.props;
 
         _removePost(id);
     }
 
     render () {
-        const { comment, created, _likePost, id, likes } = this.props;
+        const { comment, created, _likePost, id, likes, avatar, currentUserFirstName, currentUserLastName } = this.props;
 
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.post }>
-                        <span
-                            className = { Styles.cross }
-                            onClick = { this._removePost }
-                        />
-                        <img src = { context.avatar } />
-                        <a>{ context.currentUserFirstName } { context.currentUserLastName }</a>
-                        <time>{moment.unix(created).format('MMMM D h:mm:s a')}</time>
-                        <p>{ comment }</p>
-                        <Like
-                            _likePost = { _likePost }
-                            id = { id }
-                            likes = { likes }
-                            { ...context }
-                        />
-                    </section>
-                )}
-            </Consumer>
+            <section className = { Styles.post }>
+                <span
+                    className = { Styles.cross }
+                    onClick = { this._removePost }
+                />
+                <img src = { avatar } />
+                <a>{ currentUserFirstName } { currentUserLastName }</a>
+                <time>{moment.unix(created).format('MMMM D h:mm:s a')}</time>
+                <p>{ comment }</p>
+                <Like
+                    _likePost = { _likePost }
+                    id = { id }
+                    likes = { likes }
+                />
+            </section>
         );
     }
 }
